40a906d7f17f4ab6f1ac63d1b622ce51cc321f39
Mikhail Oleynik <jakiehan@yandex.ru>
2024-09-18 12:01:22 +0400

    добавить ссылку для теста

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,15 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
-import Star from '../src/assets/s1.svg?react';
-import Star3 from '../src/assets/s3.svg?react';
-import Star11 from '../src/assets/s11.svg?react';
-import Star111 from '../src/assets/s111.svg?react';
 
 function App() {
   return (
     <div>
       <div>
-        <p>Иконка 1:</p>
-        <img src={reactLogo} className="logo react" alt="React logo"/>
+        <p>Ссылка deep:</p>
+        <a href={'vk://vk.com'} target="_self" rel="noopener noreferrer">Клик</a>
       </div>
       <div>
-        <p>Иконка 2:</p>
-        <div style={{ width: '42px', height: '42px' }}>
-          <Star className="star"/>
-        </div>
-      </div>
-      <div>
-        <p>Иконка 3:</p>
-        <div style={{ width: '42px', height: '42px' }}>
-          <Star11 className="star"/>
-        </div>
-      </div>
-      <div>
-        <p>Иконка 4:</p>
-        <div style={{ width: '42px', height: '42px' }}>
-          <Star3 className="star"/>
-        </div>
-      </div>
-      <div>
-        <p>Иконка 5:</p>
-        <div style={{ width: '42px', height: '42px' }}>
-          <Star111 className="star"/>
-        </div>
+        <p>ссылка http:</p>
+        <a href={'https://vk.com/'} target="_self" rel="noopener noreferrer">Клик 2</a>
       </div>
     </div>
   )
