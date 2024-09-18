fb11e5197770841e04f75f1787c8a1b36662538f
Mikhail Oleynik <jakiehan@yandex.ru>
2024-09-18 13:54:26 +0400

    добавить ссылку в сообщения

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ function App() {
     <div>
       <div>
         <p>Ссылка deep:</p>
-        <a href={'vk://vk.com'} target="_self" rel="noopener noreferrer">Клик</a>
+        <a href={'vk://vk.com/'} target="_self" rel="noopener noreferrer">Клик</a>
       </div>
       <div>
         <p>Ссылка deep сразу в сообщения:</p>
         <a href={'vk://vk.com/im'} target="_self" rel="noopener noreferrer">Клик в сообщения</a>
+      </div>
+      <div>
+        <p>Ссылка deep 2:</p>
+        <a href={'vk://vk.com/asasas'} target="_self" rel="noopener noreferrer">Клик в сообщения</a>
       </div>
       <div>
         <p>ссылка http:</p>
